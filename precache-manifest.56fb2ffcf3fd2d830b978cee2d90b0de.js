bc7e8d7f5bdd46cb3435ce03ca9f896ef7a93b16
MeetPanchani <mmeetpanchani63@gmail.com>
2020-06-04 12:55:22 +0530

    Updates

---
+++ precache-manifest.56fb2ffcf3fd2d830b978cee2d90b0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5000b67654ac135bd9e007a982c0853",
+    "url": "/movie-series-info/index.html"
+  },
+  {
+    "revision": "c28bdeeacd2bac06220a",
+    "url": "/movie-series-info/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "e8ade3d614bab523ecdd",
+    "url": "/movie-series-info/static/js/2.aefbeba3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-series-info/static/js/2.aefbeba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28bdeeacd2bac06220a",
+    "url": "/movie-series-info/static/js/main.79247357.chunk.js"
+  },
+  {
+    "revision": "27bc1090a8cc6f2f8443",
+    "url": "/movie-series-info/static/js/runtime-main.4b3fc9cc.js"
+  }
+]);
